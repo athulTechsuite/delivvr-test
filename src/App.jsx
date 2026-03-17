2a587463bbc18ffc164ddd266f533155aa0c8660
athulTechsuite <athul@techsuite.io>
2026-03-17 17:07:31 +0530

    Add theme context provider and system theme detection to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
+import Header from './components/Header';
+import Settings from './components/Settings';
+import Home from './components/Home';
+import './styles/themes.css';
+import './App.css';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Router>
+        <div className="app">
+          <Header />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
